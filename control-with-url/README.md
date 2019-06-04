d3c061274fe3fc2ad9c334020828f3a2b46c47ed
Maggie Brewster <mcbrewst@edu.uwaterloo.ca>
2019-06-04 12:41:42 -0700

    clean

--- control-with-url/README.md
+++ control-with-url/README.md
@@ -1,9 +1,73 @@
 # Control with URL
-
-<screenshot>
+In this example upon clicking the go button, the user input will be used to generate a new url from imply that applies a page filter.
 
 To run:
 
 `npm install`
 
 `npm start`
+
+## Getting an App token
+ - Download imply quickstart and follow the quick start guide:
+  
+    https://docs.imply.io/on-prem/quickstart
+  
+ - Navigate to imply-2.9.5/conf-quickstart/pivot/config.yaml and add `enableApiEndpoint: true`
+ 
+   ![photo of settings](images/code.png "enableApiEndpoint")
+
+ - Open Localhost:9095 and from the side menu navigate to settings. UNuder setting select Api tokens and click on new token to generate an app token.
+  
+  ![photo of ui-settings](images/settings.png "ui settings")
+## Configuring a request 
+
+   You can generate links that open to specific views to update iframe by POSTing to `http://localhost:9095/api/v1/mkurl`
+
+   Requests to Imply ui requires three main components to be configured: 
+   ###x-imply-api-token:
+   This a header for the request, and should be set to the Api token generated in your local imply ui
+   
+   `"x-imply-api-token":"1a1b1cf8-fc83-495d-94d9-27f22836b81b"`
+ 
+   ###Datacube: 
+   This is the data source you are targeting. In imply ui if you select a data source the portions of the url directly after the /d/ will be the datacube value. 
+   
+   `"dataCube": "druid_wikipedia"`
+ 
+   ###Essence: 
+   The essence contains the filters you are searching by. To view the configuration add a filter in the ui and look at the payload of register under the network tab of inspect element. 
+   
+    `const essence = {
+            "dataCube": "druid_wikipedia",
+                "filter": {
+                "clauses": [
+                    {
+                        "dimension": "__time",
+                        "dynamic": {
+                            "op": "timeRange",
+                            "operand": {
+                                "op": "ref",
+                                "name": "m"
+                            },
+                            "duration": "P1D",
+                            "step": -1
+                        }
+                    },
+                    {
+                        "dimension": "page",
+                        "action" : "overlap",
+                        "exclude": false,
+                        "values": {
+                            "elements" : [req.input]
+                        },
+                        "setType": "STRING",
+                    }
+                ]
+            },`
+      
+  
+## Additional Resources 
+
+- Generating links into Imply documentation
+
+  https://docs.imply.io/on-prem/special-ui-features/generating-links-into-imply
--- control-with-url/app.js
+++ control-with-url/app.js
@@ -1,72 +1,81 @@
 const axios = require('axios');
-
 const express = require('express');
-const request = require('request-promise-native');
 var bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
-
 app.use(express.static('public'));
-
 app.use(express.json());
 
-const headers = {
+
+app.post("/mkurl", async function (req, res) {
+
+  // Set request headers
+  const headers = {
     "Content-Type":"application/json",
+    // Update Api token here
     "x-imply-api-token":"1a1b1cf8-fc83-495d-94d9-27f22836b81b"
-}
-let url;
+  };
 
-
-app.post("/mkurl", function (req, res) {
-    const essence = {
-        "dataCube": "druid_wikipedia",
-        "filter": {
-            "clauses": [
-                {
-                    "dimension": "__time",
-                    "dynamic": {
-                        "op": "timeRange",
-                        "operand": {
-                            "op": "ref",
-                            "name": "m"
-                        },
-                        "duration": "P1D",
-                        "step": -1
-                    }
-                },
-                {
-                    "dimension": "page",
-                    "action" : "overlap",
-                    "exclude": false,
-                    "values": {
-                        "elements" : [req.input]
-                    },
-                    "setType": "STRING",
-                }
-            ]
+  // Set request essence
+  const essence = {
+    "dataCube": "druid_wikipedia",
+    "filter": {
+      "clauses": [
+        {
+          "dimension": "__time",
+          "dynamic": {
+            "op": "timeRange",
+            "operand": {
+              "op": "ref",
+              "name": "m"
+            },
+            "duration": "P1D",
+            "step": -1
+          }
         },
-        timezone: "Etc/UTC",
-        "splits": [],
-        "pinnedDimensions": [],
-        "settingsVersion" : null,
-        "visualization": "totals"
-    }
+        {
+          "dimension": "page",
+          "action" : "overlap",
+          "exclude": false,
+          "values": {
+            //User Inputs updates filter
+            "elements" : [String(req.body.input)]
+          },
+          "setType": "STRING",
+        }
+      ]
+    },
+    "timezone": "Etc/UTC",
+    "splits": [],
+    "pinnedDimensions": [],
+    "selectedMeasures": ["count"],
+    "settingsVersion" : null,
+    "visualization": "totals"
+  }
 
-  axios({
-      url: 'http://localhost:9095/api/v1/mkurl',
-      method: 'post',
-      headers: headers,
-      data : {"domain": "http://localhost:9095", "essence": essence}
-  }).then(function (response) {
-      url = response.data.url;
+  // Send request to imply-ui api endpoint
+  let response = await axios({
+    url: 'http://localhost:9095/api/v1/mkurl',
+    method: 'post',
+    headers: headers,
+    data : {"domain": "http://localhost:9095", "essence": essence}
   })
-      .catch(function (error) {
-      });
-  res.send({
-    "url": url
-  })
+
+  // Update and send Url
+  let url = response.data.url;
+  if (url) {
+    res.send({
+      "url": url
+    });
+  }
+  // If no url return error
+  else {
+    res.status(500).send({
+      error: 'could not make url'
+    });
+  }
 });
 
 app.listen(port, () => {
