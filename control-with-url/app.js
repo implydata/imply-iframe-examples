c160b805bf013215f5dbea6e6a7d9e1189981f90
Maggie Brewster <mcbrewst@edu.uwaterloo.ca>
2019-06-03 23:41:58 -0700

    add example

--- control-with-url/app.js
+++ control-with-url/app.js
@@ -1,10 +1,71 @@
+const axios = require('axios');
+
 const express = require('express');
 const request = require('request-promise-native');
+var bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
+
 app.use(express.static('public'));
+
+app.use(express.json());
+
+const headers = {
+    "Content-Type":"application/json",
+    "x-imply-api-token":"1a1b1cf8-fc83-495d-94d9-27f22836b81b"
+}
+let url;
+const essence = {
+    "dataCube": "druid_wikipedia",
+        "filter": {
+        "clauses": [
+            {
+                "dimension": "__time",
+                "dynamic": {
+                    "op": "timeRange",
+                    "operand": {
+                        "op": "ref",
+                        "name": "m"
+                    },
+                    "duration": "P1D",
+                    "step": -1
+                }
+            },
+            {
+                "dimension": "page",
+                "action" : "overlap",
+                "exclude": false,
+                "values": {
+                    "elements" : [req.input]
+                },
+                "setType": "STRING",
+            }
+        ]
+    },
+    timezone: "Etc/UTC",
+    "splits": [],
+        "pinnedDimensions": [],
+        "settingsVersion" : null,
+        "visualization": "totals"
+}
+
+app.post("/mkurl", function (req, res) {
+  axios({
+      url: 'http://localhost:9095/api/v1/mkurl',
+      method: 'post',
+      headers: headers,
+      data : {"domain": "http://localhost:9095", "essence": essence}
+  }).then(function (response) {
+      url = response.data.url;
+  })
+      .catch(function (error) {
+      });
+  res.send({
+    "url": url
+  })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
--- control-with-url/public/logic.js
+++ control-with-url/public/logic.js
@@ -1,5 +1,26 @@
-console.log('aaa');
-document.querySelector('#go').addEventListener('click', function() {
-  console.log('here');
-  document.querySelector('iframe').src = "http://localhost:9095/pivot/d/7f939cdefbc5bf95c7/Wikipedia";
+
+document.querySelector('#go').addEventListener('click', function(e) {
+  let request = {
+    'input': document.getElementById('input').value
+  };
+  let url = 'http://localhost:9095/pivot/d/7ddc63f3f64b0aeb52/Wikipedia'
+
+  fetch('/mkurl', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(request)
+  }).then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    if(url){
+      url = json.url
+    }
+  });
+
+  document.getElementById('iframe').src = url;
+
 });
